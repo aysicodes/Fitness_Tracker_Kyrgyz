c4031c9467f056b4d91d95364233223af0dd516a
aizirek19 <aizirek580@gmail.com>
2025-12-23 16:00:34 +0600

    fix: update security config and ensure all dependencies are pushed

--- src/main/java/com/example/fit_tracker/config/WebSecurityConfig.java
+++ src/main/java/com/example/fit_tracker/config/WebSecurityConfig.java
@@ -1,9 +1,9 @@
 package com.example.fit_tracker.config;
 
+
 import com.example.fit_tracker.security.AuthEntryPointJwt;
 import com.example.fit_tracker.security.AuthTokenFilter;
 import com.example.fit_tracker.security.UserDetailsServiceImpl;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
